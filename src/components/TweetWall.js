c5cb839d02df1c3a5dfe3a48ed52b1817237246f
gnappo1 <gnappo.1@live.it>
2018-01-30 19:02:54 -0800

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -11,8 +11,25 @@ class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    });
+  }
+
   // TODO: shouldComponentUpdate()
+  shouldComponentUpdate(nextProps, nextState) {
+    return (nextProps.newTweets.length > 0)
+  }
+
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps(nextProps) {
+    var array_1 = nextProps.newTweets;
+    var array_2 = this.state.tweets;
+    this.setState({
+      tweets: array_1.concat(array_2)
+    });
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
